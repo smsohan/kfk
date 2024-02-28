3d81984da626077e365f658ca566ba6a37ae451d
Sohan SM <sohansm@google.com>
2024-02-28 19:24:18 +0000

    got keda working

--- gke/bootstrap.sh
+++ gke/bootstrap.sh
@@ -11,7 +11,12 @@ TOPIC=gke-test-topic
 gcloud container clusters get-credentials $NAME-cluster \
     --location $LOCATION
 
-kubectl apply -f deployment.yml
+kubectl apply --server-side -f https://github.com/kedacore/keda/releases/download/v2.13.0/keda-2.13.0.yaml
 
+
+kubectl apply -f app-deployment.yml
+kubectl apply -f consumer-deployment.yml
+
+kubectl port-forward <app-pod-id> 8080:8080
 
 # /opt/bitnami/kafka/bin/kafka-console-producer.sh --bootstrap-server 127.0.0.1:9092 --producer.config /opt/bitnami/kafka/config/producer.properties --topic gke-test-topic-2
